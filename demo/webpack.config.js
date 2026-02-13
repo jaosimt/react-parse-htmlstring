d0ba70a3ef746d80338b78cdd67c3abb7747bc60
Sim Jao <jao.sim@gmail.com>
2026-02-13 12:12:50 +0800

    * replace node-sass with sass (dartSass)

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
 			},
 			{
 				test: /\.(scss|css)$/,
-				use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+				use: [ MiniCssExtractPlugin.loader, 'css-loader' ]
 			},
 		]
 	},
